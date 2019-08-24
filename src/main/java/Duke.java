9616725dd5e81c50c9613488e98b73de1cdfc41a
Valerie Tan <valerie.tan@u.nus.edu>
2019-08-24 11:38:47 +0800

    level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,28 +2,50 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        String[] listArray = new String[100];
+        String input;
+        int count = 0;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-
         System.out.println("________________________________");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println("________________________________");
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
 
-        if (input.equals("bye")) {
-            System.out.println("Bye. Hope to see you again soon!");
+        do {
+            Scanner sc = new Scanner(System.in);
+            input = sc.nextLine();
+
+            if (input.equals("list")) {
+                break;
+            }
+            if (input.equals("bye")) {
+                break;
+            }
+
+            System.out.println("added: " + input);
+            listArray[count] = input;
+            count++;
+
+        } while (!input.isEmpty());
+
+
+        if (input.equals("list")) {
+            for (int i = 0; i < count; i++) {
+                System.out.println((i+1) + ". " + listArray[i]);
+
+            }
+            System.out.println("\nBye. Hope to see you again soon!");
         }
-        else {
-            System.out.println(input);
+        else if (input.equals("bye"))
+        {
+            System.out.println("Bye. Hope to see you again soon!");
         }
 
     }
-
 
 }
