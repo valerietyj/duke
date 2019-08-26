288ce9b33beea05a7d65012fe29117651c5bdca8
Valerie Tan <valerie.tan@u.nus.edu>
2019-08-26 19:35:28 +0800

    level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
 
 public class Duke {
+
     public static void main(String[] args) {
+
         String[] listArray = new String[100];
+        String[] yesNO = new String[100];
         String input;
         int count = 0;
         String logo = " ____        _        \n"
@@ -19,13 +22,30 @@ public class Duke {
         do {
             Scanner sc = new Scanner(System.in);
             input = sc.nextLine();
+            String s = input;
+            String[] parts = s.split(" ");
+            String taskDone = parts[0];
 
-            if (input.equals("list")) {
+            if (taskDone.equals("done")) {
+                // System.out.println(parts[1]);
+                int list = Integer.parseInt(parts[1]);
+                Task t = new Task(list - 1, listArray[list-1]);
+                t.markAsDone(true);
+               // String status = TaskStatus(list - 1 , listArray[list-1], "done");
+                yesNO[list - 1] = t.getStatusIcon();
+                System.out.println("[" + t.getStatusIcon() + "] " + listArray[list - 1]);
+
+            }
+            else if (input.equals("list")) {
                 if (count == 0) {
                     System.out.println("nothing in list");
                 }
+
                 for (int i = 0; i < count; i++) {
-                    System.out.println((i + 1) + ". " + listArray[i]);
+                    //String status = TaskStatus(i, listArray[i], "list");
+                    //Task t = new Task(i , listArray[i]);
+                   // System.out.println((i + 1) + ". " + listArray[i]);
+                    System.out.println((i + 1) + ". [" + yesNO[i] + "] " + listArray[i]);
                 }
                 continue;
             }
@@ -37,10 +57,29 @@ public class Duke {
             {
                 System.out.println("added: " + input);
                 listArray[count] = input;
+                Task t = new Task(count, input);
+                yesNO[count]=t.getStatusIcon();
                 count++;
             }
 
         } while (!input.isEmpty());
 
     }
+
+  /*  public static String TaskStatus(int input, String name, String status) {
+        Task t = new Task(input, name);
+
+
+        if (status.equals("done"))
+        {
+            t.markAsDone(true);
+            System.out.println("Nice! I've marked this task as done: ");
+            return t.getStatusIcon();
+        }
+        else if (status.equals("list")){
+            return t.getStatusIcon();
+        }
+        return "error";
+    }*/
+
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,8 +1,11 @@
 public class Task {
     protected String description;
     protected boolean isDone;
+    public int key;
 
-    public Task(String description) {
+
+    public Task(int id, String description) {
+        this.key = id;
         this.description = description;
         this.isDone = false;
     }
@@ -11,5 +14,9 @@ public class Task {
         return (isDone ? "YES" : "NO"); //return tick or X symbols
     }
 
-    //...
+    public void markAsDone(boolean value) {
+        this.isDone = value;
+
+    }
+
 }
